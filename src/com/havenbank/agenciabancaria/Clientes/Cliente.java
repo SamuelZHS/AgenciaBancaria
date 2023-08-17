89eac3e39d0e30ca1a1ad2423d8d751514dd43d5
unknown <samuel.heleno.azevedo@gmmail.com>
2023-08-17 13:41:55 -0300

    Integração de classe main para testar objeto cliente

--- src/com/havenbank/agenciabancaria/Clientes/Cliente.java
+++ src/com/havenbank/agenciabancaria/Clientes/Cliente.java
@@ -1,2 +1,40 @@
-package com.havenbank.agenciabancaria.Clientes;public class Cliente {
+package com.havenbank.agenciabancaria.Clientes;
+
+import java.util.Scanner;
+
+public class Cliente {
+
+    Scanner  inputDate = new Scanner(System.in);
+    private String nomeCompleto;
+    private String emailCl;
+    private int CPF;
+    private int password;
+    private int confirmPassword;
+
+
+    public Cliente(String nomeCompleto, String emailCl, int CPF) {
+        this.nomeCompleto = nomeCompleto;
+        this.emailCl = emailCl;
+        this.CPF = CPF;
+    }
+
+    public String cadastrar(String nomeCompleto, String emailCl, int CPF){
+        System.out.print("Nome completo:");
+        nomeCompleto = inputDate.nextLine();
+        System.out.print("Email:");
+        emailCl = inputDate.nextLine();
+        System.out.print("CPF:");
+        CPF = inputDate.nextInt();
+        System.out.print("Senha:");
+        password = inputDate.nextInt();
+        System.out.print("Confirmar Senha:");
+        confirmPassword = inputDate.nextInt();
+
+        if (confirmPassword == password){
+            System.out.println("Conta cadastrada com sucesso");
+        }else {
+            System.out.println("As duas senhas precisam ser iguais!");
+        }
+          return "...";
+    }
 }
---
+++ src/com/havenbank/agenciabancaria/Clientes/Main.java
@@ -0,0 +1,4 @@
+package com.havenbank.agenciabancaria.Clientes;
+
+public class Main {
+}
